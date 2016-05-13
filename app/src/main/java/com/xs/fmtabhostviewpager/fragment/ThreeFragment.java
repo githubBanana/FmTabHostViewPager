967b2d3043708a2123158064641170cb1cc02994
siushen <784843867@qq.com>
2016-05-13 17:33:05 +0800

    add nineoldandroid animator test

--- app/src/main/java/com/xs/fmtabhostviewpager/fragment/ThreeFragment.java
+++ app/src/main/java/com/xs/fmtabhostviewpager/fragment/ThreeFragment.java
@@ -5,9 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.nineoldandroids.animation.AnimatorSet;
+import com.nineoldandroids.animation.ObjectAnimator;
+import com.nineoldandroids.view.ViewHelper;
 import com.xs.fmtabhostviewpager.R;
 
 /**
@@ -19,6 +23,7 @@ import com.xs.fmtabhostviewpager.R;
 public class ThreeFragment extends Fragment {
     private static final String TAG = "ThreeFragmetn";
 
+    private View mTouchView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -37,9 +42,33 @@ public class ThreeFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         Log.e(TAG,"onViewCreated");
-
+        mTouchView = view.findViewById(R.id.view_touchId);
+        mTouchView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    touch();
+                }
+                return true;
+            }
+        });
     }
 
+    private void touch() {
+        //设置中心点为中间点
+        /*ViewHelper.setPivotX(mTouchView, mTouchView.getWidth() / 2f);
+        ViewHelper.setPivotY(mTouchView, mTouchView.getHeight() / 2f);*/
+        //1.创建动画集
+        AnimatorSet set = new AnimatorSet();
+        //2.添加动画
+        set.setupEndValues();
+        set.playTogether(
+                ObjectAnimator.ofFloat(mTouchView, "scaleX", 2, 1.5f, 1).setDuration(1000),
+                ObjectAnimator.ofFloat(mTouchView, "scaleY", 2, 1.5f, 1).setDuration(1000),
+                ObjectAnimator.ofFloat(mTouchView, "alpha", 0, 1).setDuration(1000 * 3 / 2));
+        //3.开启动画
+        set.start();
+    }
     @Override
     public void onStart() {
         super.onStart();
